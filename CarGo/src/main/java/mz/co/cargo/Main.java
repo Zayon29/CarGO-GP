ea9073a5ab5fe7f7f386e919486797cbff9d940b
JPed552 <jpbandeira2011@hotmail.com>
2025-03-27 17:57:33 -0300

    ajustes das telas

--- CarGo/src/main/java/mz/co/cargo/Main.java
+++ CarGo/src/main/java/mz/co/cargo/Main.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("cadastro-usuario.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1249, 830);
-        stage.setTitle("Login - CarGo");
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login-usuario.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1400, 900);
+        stage.setTitle("Sistema de Locação de veículos - CarGo");
         stage.setScene(scene);
         stage.setResizable(false); // Impede redimensionamento
         stage.show();
